c8b2fa1c08b8c4f8c6ae5aa51e5412a09d34bc8e
tstatler <tstatler@cisco.com>
2022-11-02 16:21:12 -0700

    Edits

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Browser SDK Messaging Sample app
 
-This sample shows how to use the Webex Browser SDK to make API call and display results. It accompanies a [tutorial](https://developer.webex.com/docs/browser-sdk-messaging-tutorial) on the Webex Developer Portal that you can follow along with.
+This sample shows how to use the Webex Browser SDK to do the following:
+
+* List rooms (spaces) you belong to
+* Create and delete rooms
+* Send a message to a room
+* Add members to a room
+* Listen for newly created or deleted messages in a room
+
+This app accompanies a [tutorial](https://developer.webex.com/docs/browser-sdk-messaging-tutorial) on the Webex Developer Portal that you can follow along with with [starter files](./starter), or run the [completed project](./completed).
 
 ![](app.png)
 
 To use the completed app you need to obtain use your [personal access token](https://developer.webex.com/docs/getting-your-personal-access-token) from the Developer Portal that the SDK will use to make API calls.
 
-> In a production environment you will want to create a Webex Integration (OAuth client) that will obtain an API access token for the authenticating Webex user.
+> In a production environment you will want to create a [Webex Integration](https://developer.webex.com/docs/integrations) (OAuth client) that will obtain an API access token for the authenticating Webex user.
 
 Add your personal access token to index.js:
 
---
+++ completed/index.js
@@ -0,0 +1,168 @@
+// Get your personal access token:
+// https://developer.webex.com/docs/getting-started
+
+var personalAccessToken="Yjg3YThlODctZjFhYy00NjViLWFlNWEtZTkwYjUwNTQ4ZjcwNzdhZTNmYWMtMmRi_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f";
+
+const webex = (window.webex = window.Webex.init({
+  credentials: {
+    access_token: personalAccessToken
+  }
+}));
+
+// Text area for logging output
+var output = document.getElementById("output");
+
+webex.once(`ready`, function () {
+  if (webex.canAuthorize) {
+    log("Authorization successful.");
+  }
+});
+
+
+// List rooms
+
+function listRooms() {
+  output.value = "";
+  webex.rooms
+    .list({ max: 10 })
+    .then(function (rooms) {
+      if (rooms.items.length == 0) {
+        log("There are no rooms to list. Click Create Room.");
+      } else {
+        for (var i = 0; i < rooms.items.length; i += 1) {
+          log(`${rooms.items[i].title} (${rooms.items[i].id})`);
+        }
+      }
+    })
+    .catch(function (error) {
+      log(error);
+    });
+}
+
+// ID and name for last room created via API
+let newRoomName, newRoomId;
+
+function createRoom() {
+  // Prompt user for room name:
+  
+  newRoomName = prompt("Enter room name", "My Test Room");
+    webex.rooms
+      .create({ title: newRoomName })
+      .then(function (room) {
+        output.value = "";
+        newRoomId = room.id;
+        log(`Created room: ${room.title} with ID ${room.id}`);
+      })
+      .catch(function (error) {
+        output.value += error + "\n";
+      });
+}
+
+// Send message to room
+
+function sendMessage() {
+  // Clear log
+  output.value = "";
+  
+  // Prompt user for message
+  let msgText = prompt("Enter message text", "Hi from the Webex JS SDK!");
+
+  // Use ID of last room created with Create Room
+  if (newRoomId) {
+    webex.messages
+      .create({
+        text: msgText,
+        roomId: newRoomId,
+      })
+      .then(function (message) {
+        log(`Message sent to ${newRoomName}`);
+      })
+      .catch(function (error) {
+        log(`Error sending message: ${error}`);
+      });
+  } else {
+    log("No room ID provided. Click Create Room, first.");
+  }
+}
+
+
+// Add a member to a room:
+
+function addMember() {
+  
+  // Prompt user for email of member
+  let email = prompt("Enter email of user to add", "user@example.com");
+
+  if (newRoomId) {
+    webex.memberships
+      .create({
+        personEmail: email,
+        roomId: newRoomId,
+      })
+      .then(function (membership) {
+        log(`${membership.personEmail} added to room`);
+      })
+      .catch(function (error) {
+        log(error);
+      });
+  } else {
+    log("No room ID. First, click Create Room.");
+  }
+}
+
+// Delete room by ID
+
+function deleteRoom() {
+  // Prompt user for room ID
+  let roomId = prompt("Enter ID of room to delete", newRoomId);
+
+  webex.rooms
+    .remove(roomId)
+    .then(function () {
+      output.value = "";
+      log("Room deleted");
+    })
+    .catch(function (error) {
+      log(" ");
+      log(`Error deleting room ${error}`);
+    });
+}
+
+
+// Start listening for new/delete messages events
+
+function startListening() {
+  webex.messages
+    .listen()
+    .then(() => {
+      webex.messages.on("created", (event) =>
+        log(`Got a message:created event: ${JSON.stringify(event)}`)
+      );
+      webex.messages.on("deleted", (event) =>
+        log(`Got a message:deleted event: ${JSON.stringify(event)}`)
+      );
+      output.value = "";
+      log("Listening for new/deleted messages...");
+    })
+    .catch((e) => console.error(`Unable to register for message events: ${e}`));
+}
+
+// Stop listening for events
+
+function stopListening() {
+  webex.messages.stopListening();
+  webex.messages.off("created");
+  webex.messages.off("deleted");
+  log("Stopped listening for new/deleted messages.");
+}
+
+
+// For doing OAuth-based flow
+// function startLogin() {
+//   webex.authorization.initiateLogin();
+// }
+
+// Log utility function
+function log(data) {
+  output.value += data += "\n";
+}
